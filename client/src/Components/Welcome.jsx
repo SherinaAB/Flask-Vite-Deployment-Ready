9eb6a0b8a89a9a11fb54e890ab032a70d807b86d
Sherina Buenaseda <sherina76@gmail.com>
2023-09-12 00:54:33 -0600

    organized components and added working file folder for Gets, Posts, Patches, and Deletes

---
+++ client/src/Components/Welcome.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom";
+
+
+export default function WelcomePage() {
+
+
+  return (
+    <div>
+        <> WELCOME PAGE w/ Date Selection Drop-down or Buttons </>
+    </div>
+  )
+}
